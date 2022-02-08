386120156f0c5ffcf36acf556e2614581af515e0
nurdinahmadalawiyah <nurdinahmada@gmail.com>
2022-02-08 17:09:19 +0700

    Make Custom Button Working

--- app/src/main/java/com/nurdin/mycustomview/MyButton.kt
+++ app/src/main/java/com/nurdin/mycustomview/MyButton.kt
@@ -2,23 +2,49 @@ package com.nurdin.mycustomview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
+import android.view.Gravity
 import androidx.appcompat.widget.AppCompatButton
+import androidx.core.content.ContextCompat
 
 class MyButton : AppCompatButton {
-    constructor(context: Context) : super(context) {
 
+    private var enableBackground: Drawable? = null
+    private var disableBackground: Drawable? = null
+    private var txtColor: Int = 0
+
+    constructor(context: Context) : super(context) {
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
 
+    private fun init() {
+        txtColor = ContextCompat.getColor(context, android.R.color.background_light)
+        enableBackground = ContextCompat.getDrawable(context, R.drawable.bg_button)
+        disableBackground = ContextCompat.getDrawable(context, R.drawable.bg_button_disable)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        background = when {
+            isEnabled -> enableBackground
+            else -> disableBackground
+        }
+
+        setTextColor(txtColor)
+        textSize = 12f
+        gravity = Gravity.CENTER
+        text = when {
+            isEnabled -> "Submit"
+            else -> "Isi Dulu"
+        }
     }
 }
